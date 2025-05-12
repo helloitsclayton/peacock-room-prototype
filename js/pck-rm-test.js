8efde29348f401c461dc909c51a462659f17971d
Clayton Hayes <7267647+helloitsclayton@users.noreply.github.com>
2025-05-12 14:13:59 +0000

    Add files via upload

---
+++ js/pck-rm-test.js
@@ -0,0 +1,75 @@
+var firstTour = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-299.4927779207251,
+					201.625
+				]
+			},
+			"properties": {
+				"name": "Detroit",
+				"id": "detroit"
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					234.37377443503215,
+					238.125
+				]
+			},
+			"properties": {
+				"name": "Florence",
+				"id": "florence"
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					226.62648818603242,
+					275.875
+				]
+			},
+			"properties": {
+				"name": "Paris",
+				"id": "paris"
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					451.2148087773511,
+					88.625
+				]
+			},
+			"properties": {
+				"name": "Bombay",
+				"id": "bombay"
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					694.8789830861456,
+					205.875
+				]
+			},
+			"properties": {
+				"name": "Kyoto",
+				"id": "kyoto"
+			}
+		}
+	]
+};
